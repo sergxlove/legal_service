10a9f183d0d19597bbbdbd1c1298f32a1b35f7e8
sergxlove <sergxlove@mail.ru>
2023-12-03 01:52:33 +0600

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+class legal_service
+{
+public:
+private:
+	int number;//номер услуги
+	int category;//категория услуги
+	string name_service;//название услуги
+	string date;//дата
+	string name_client;//фио клиента
+	string name_lawyer;//фио юриста
+	int price;//стоимость
+};
+int main()
+{
+	return 0;
+}
