47c25d15dc37cb2cff9a70c7a69cb0738409e033
sergxlove <sergxlove@mail.ru>
2023-12-03 20:26:52 +0600

    add struct programm

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,9 @@ using namespace std;
 class legal_service
 {
 public:
+	legal_service();
+	legal_service(int number, int category, string name_service, string date, string name_client, string name_lawyer, int price);
+	~legal_service();
 private:
 	int number;//номер услуги
 	int category;//категория услуги
@@ -17,5 +20,73 @@ private:
 };
 int main()
 {
+	setlocale(LC_ALL, "rus");
+	legal_service service;
+	vector<legal_service> arr;
+	int var_switch;
+	var_switch = 0;
+	bool exit = false;
+	while (!exit)
+	{
+		switch (var_switch)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			exit = true;
+			break;
+		default:
+			break;
+		}
+	}
 	return 0;
+}
+
+legal_service::legal_service()
+{
+	this->number = 0;
+	this->category = 0;
+	this->name_service = "";
+	this->date = "";
+	this->name_client = "";
+	this->name_lawyer = "";
+	this->price = 0;
+}
+
+legal_service::legal_service(int number, int category, string name_service, string date, string name_client, string name_lawyer, int price)
+{
+	this->number = number;
+	this->category = category;
+	this->name_service = name_service;
+	this->date = date;
+	this->name_client = name_client;
+	this->name_lawyer = name_lawyer;
+	this->price = price;
+}
+
+legal_service::~legal_service()
+{
+	this->number = 0;
+	this->category = 0;
+	this->name_service = "";
+	this->date = "";
+	this->name_client = "";
+	this->name_lawyer = "";
+	this->price = 0;
 }
